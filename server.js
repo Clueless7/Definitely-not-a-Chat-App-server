06c8dd97273fc9d06b3771b2071a966efbb19e03
Josel Catalan <catalanjosel123@gmail.com>
2023-01-31 19:21:01 +0800

    set cookie to lax

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.use(
     resave: false,
     cookie: {
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: 'lax',
       // secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
