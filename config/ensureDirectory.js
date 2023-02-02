ca2518e3f4e9e7dbe9b47a84524c1c7bbe41b276
Nestortion <gerona.nestor@gmail.com>
2023-02-03 00:22:16 +0800

    updated path

--- config/ensureDirectory.js
+++ config/ensureDirectory.js
@@ -3,10 +3,10 @@ import path from 'node:path';
 
 
 export default function() {
-    existsSync(path.join('__dirname', "./files")) || mkdirSync(path.join('__dirname', "./files"));
-    existsSync(path.join('__dirname', "./files/pfp")) || mkdirSync(path.join('__dirname', "./files/pfp"));
-    existsSync(path.join('__dirname', "./files/grouppfp")) || mkdirSync(path.join('__dirname', "./files/grouppfp"));
-    existsSync(path.join('__dirname', "./files/message")) || mkdirSync(path.join('__dirname', "./files/message"));
-    existsSync(path.join('__dirname', "./files/message/images")) || mkdirSync(path.join('__dirname', "./files/message/images"));
-    existsSync(path.join('__dirname', "./files/message/documents")) || mkdirSync(path.join('__dirname', "./files/message/documents"));
+    existsSync(path.join('__dirname', "../files")) || mkdirSync(path.join('__dirname', "../files"));
+    existsSync(path.join('__dirname', "../files/pfp")) || mkdirSync(path.join('__dirname', "../files/pfp"));
+    existsSync(path.join('__dirname', "../files/grouppfp")) || mkdirSync(path.join('__dirname', "../files/grouppfp"));
+    existsSync(path.join('__dirname', "../files/message")) || mkdirSync(path.join('__dirname', "../files/message"));
+    existsSync(path.join('__dirname', "../files/message/images")) || mkdirSync(path.join('__dirname', "../files/message/images"));
+    existsSync(path.join('__dirname', "../files/message/documents")) || mkdirSync(path.join('__dirname', "../files/message/documents"));
 }
