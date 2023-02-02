0a4b1025ba5194a17f8a739f36c39ce89a14df75
Nestortion <gerona.nestor@gmail.com>
2023-02-03 00:16:42 +0800

    Ensure files path exist or created

---
+++ config/ensureDirectory.js
@@ -0,0 +1,11 @@
+import { existsSync, mkdirSync } from 'node:fs'
+
+
+export default function() {
+    existsSync(path.join('__dirname', "./files")) || mkdirSync(path.join('__dirname', "./files"));
+    existsSync(path.join('__dirname', "./files/pfp")) || mkdirSync(path.join('__dirname', "./files/pfp"));
+    existsSync(path.join('__dirname', "./files/grouppfp")) || mkdirSync(path.join('__dirname', "./files/grouppfp"));
+    existsSync(path.join('__dirname', "./files/message")) || mkdirSync(path.join('__dirname', "./files/message"));
+    existsSync(path.join('__dirname', "./files/message/images")) || mkdirSync(path.join('__dirname', "./files/message/images"));
+    existsSync(path.join('__dirname', "./files/message/documents")) || mkdirSync(path.join('__dirname', "./files/message/documents"));
+}
